6036112d0456284b29b6ad5b2c993430b77106dc
Neveous <badyrova.kristin@mail.ru>
2023-02-25 21:27:12 +0800

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 8415; // стоимость билета
+        int amount = 20; // количество рублей для одной бонусной мили
+        System.out.println("Количество начисленных миль за купленный билет:");
+        System.out.println((price / amount) + " бонусных миль");
+        
+
+
+
+    }
+
+}
